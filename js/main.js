6e18639be556c41b70d6457dce006661cacd4bc9
mitriadamian <damian.mitria@gmail.com>
2022-07-01 00:35:33 -0300

    Mi carrito falta terminar unas cosas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
-$(function() {
-    // Handler for .ready() called.
-  });
+$(function () {
+    $('#input1').on('change', function () {
+        var valor = this.value;
+
+        if (valor == 1) {
+            $('#precio1').text("3");
+        } else if (valor == 2) {
+            $('#precio1').text("5");
+        } else if (valor == 0) {
+            $('#precio1').text("0");
+        }
+        else {
+            $('#precio1').text("6");
+        }
+
+   
+    });
+
+
+    $('#cancelar1').on('click', function () {
+
+        $('#item1').hide();
+
+    });
+
+
+
+    $('#input2').on('change', function () {
+        var valor = this.value;
+
+        if (valor == 1) {
+            $('#precio2').text("3");
+        } else if (valor == 2) {
+            $('#precio2').text("5");
+        } else if (valor == 0) {
+            $('#precio2').text("0");
+        }
+        else {
+            $('#precio2').text("6");
+        }
+    });
+
+
+    $('#cancelar2').on('click', function () {
+
+        $('#item2').hide();
+
+    });
+
+
+
+
+
+});
